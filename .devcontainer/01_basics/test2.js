c9f9e2dcc1c123174a6ac240547dcd2e01c5da54
ShubhamDhiman4133 <shubham662002@gmail.com>
2024-08-31 05:03:02 +0000

    Datatypes & types of datatypes

---
+++ .devcontainer/01_basics/test2.js
@@ -0,0 +1,19 @@
+let customerId =3245
+let customerEmail="shubh@gmail.com"
+let customerCity = false
+let outsideTemp = null
+let bigNumber = 3456789997353n
+const nameOf = Symbol('3256')
+let username
+
+let myFunc = {
+    name:"Shubham",
+    age:23,
+    city: false,
+}
+
+const myFunction = function() {
+    console.log("hello world");
+}
+
+console.table([typeof customerId,typeof customerEmail,typeof customerCity,typeof outsideTemp,typeof bigNumber,typeof nameOf,typeof username,typeof myFunc,typeof myFunction]);
