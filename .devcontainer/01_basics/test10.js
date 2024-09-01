8c8cadc5ba1e410bd2b52ef45b6dab616f1d8407
ShubhamDhiman4133 <shubham662002@gmail.com>
2024-09-01 15:25:42 +0000

    Control Flow in JS

--- .devcontainer/01_basics/test10.js
+++ .devcontainer/01_basics/test10.js
@@ -1,47 +1,48 @@
-// if
+ //if
 const isUserloggedIn = true
 const temperature = 41
 
-// if ( temperature === 40 ){
-//     console.log("less than 50");
-// } else {
-//     console.log("temperature is greater than 50");
-// }
+ if ( temperature === 40 ){
+    console.log("less than 50");
+ } else {
+     console.log("temperature is greater than 50");
+ }
 
-// console.log("Execute");
-// <, >, <=, >=, ==, !=, ===, !==
+    console.log("Execute");
+   // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+ const score = 200
 
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
+ if (score > 100) {
+     let power = "fly"
+     console.log(`User power: ${power}`);
+ }
 
-// console.log(`User power: ${power}`);
+//console.log(`User power: ${power}`);
 
 
-// const balance = 1000
+ const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+ if (balance > 500) console.log("test"),console.log("test2");
 
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
+ if (balance < 500) {
+     console.log("less than 500");
+ } else if (balance < 750) {
+     console.log("less than 750");
     
-// } else if (balance < 900) {
-//     console.log("less than 750");
+ } else if (balance < 900) {
+     console.log("less than 750");
     
-// } else {
-//     console.log("less than 1200");
+ } else {
+     console.log("less than 1200");
 
-// }
+ }
 
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
+
+ const userLoggedIn = true
+ const debitCard = true
+ const loggedInFromGoogle = false
+ const loggedInFromEmail = true
 
 if (userLoggedIn && debitCard && 2==3) {
     console.log("Allow to buy course");
@@ -51,35 +52,29 @@ if (loggedInFromGoogle || loggedInFromEmail) {
     console.log("User logged in");
 }
 
-// switch (key) {
-//     case value:
+let mont =3
+ switch (mont) {
+    case 1:
+        console.log("Jan")
+     break;
+
+    case 2:
+           console.log("Feb")
+        break;
+
+    case 3:
+               console.log("March")
+            break;  
+
+    case 4:
+       console.log("April")
+    break; 
+     
+     default:
+        console.log("Nothing Found");
         
-//         break;
-
-//     default:
-//         break;
-// }
-
-const month = "march"
-
-switch (month) {
-    case "jan":
-        console.log("January");
-        break;
-    case "feb":
-        console.log("feb");
-        break;
-    case "march":
-        console.log("march");
-        break;
-    case "april":
-        console.log("april");
-        break;
-
-    default:
-        console.log("default case match");
-        break;
-}
+         break;
+ }
 
 const userEmail = []
 
@@ -96,9 +91,9 @@ if (userEmail) {
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
 
-// if (userEmail.length === 0) {
-//     console.log("Array is empty");
-// }
+ if (userEmail.length === 0) {
+     console.log("Array is empty");
+ }
 
 const emptyObj = {}
 
