e252620ac2045bd299b44dc1815a509f2b7926cc
ShubhamDhiman4133 <shubham662002@gmail.com>
2024-09-01 09:22:54 +0000

    Global_and_LocalScope in JS

---
+++ .devcontainer/01_basics/test7.js
@@ -0,0 +1,15 @@
+let a = 70
+const b = 80
+var c = 90
+
+if (true){
+    let a =7
+    const b = 9
+    var c = 5 // DUE TO THIS DONT USE VAR 
+}
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
